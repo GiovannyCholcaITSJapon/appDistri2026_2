49a1ffe1c284869f266a1d06d451e398faaf491b
Giovanny Cholca <byroncholca@gmail.com>
2026-04-11 13:39:47 -0500

    anotacion para clave primaria

--- appNetCore/app.ecCliente.api/app.ecCliente.entities/models/EntidadesModel.cs
+++ appNetCore/app.ecCliente.api/app.ecCliente.entities/models/EntidadesModel.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace app.ecCliente.entities.models
 {
     public class EntityBase
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public bool Estado { get; set; }
         public DateTime Fecha { get; set; }
